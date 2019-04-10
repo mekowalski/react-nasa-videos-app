56e54350e7874265f7c00015dce924fb51243e6e
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-10 10:31:22 -0600

    create, add /components and /App

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        This is a NASA Videos App
+      </div>
+    )
+  }
+}
+
+export default App

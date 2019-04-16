e5187f932744402326b64df9e770766e6f6f0296
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-16 16:18:00 -0600

    add center and date

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -10,6 +10,8 @@ const VideoDetail = ({ title }) => {
       <div className='ui segment'>
         <img className='ui fluid image' src={title.links[0].href} />
         <h3 className='ui header'>{title.data[0].title}</h3>
+        <p>Center: {title.data[0].center}</p>
+        <p>Date: {title.data[0].date_created}</p>
         <p>{title.data[0].description}</p>
       </div>
     </div>

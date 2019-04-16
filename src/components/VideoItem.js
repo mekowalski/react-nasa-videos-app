74bf358c95cede439e0a81f6cb65f6b1d8638262
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-16 14:28:29 -0600

    thumbnail images worked perfectly!!!

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,6 +3,7 @@ import React from 'react';
 const VideoItem = ({ title }) => {
   return (
     <div>
+      <img src={title.links[0].href} />
       {title.data[0].title}
     </div>
   )
@@ -11,4 +12,7 @@ const VideoItem = ({ title }) => {
 export default VideoItem
 
 //what properties to refer to inside the object
-//ref path: title.data.title to print out the title of each item
+//ref path: title.data[0].title to print out the title of each item
+
+//try to display an image
+//ref path: title.links[0].href to get the thumbnail image
